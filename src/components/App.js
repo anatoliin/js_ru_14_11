658d9a076e3dcd504c85bff23c898b90c02a3ff3
Roman <kaboom.ua@gmail.com>
2016-11-28 11:42:06 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import 'react-select/dist/react-select.css'
 import DayPicker, { DateUtils } from "react-day-picker"
 import "react-day-picker/lib/style.css"
 import moment from 'moment'
-
+//DayPicker стоит вынести в отдельный компонент
 class App extends Component {
 
     constructor(props) {
